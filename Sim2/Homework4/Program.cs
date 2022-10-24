f2363cfbb2f3ba572005bd5d026a58ec60bc9fd0
Geraschenco Vladislav <xrawen3@gmail.com>
2022-10-24 23:53:14 +0300

    added Homework3 Sim to two

---
+++ Sim2/Homework4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int namber = Convert.ToInt32(Console.ReadLine());
+    return namber;
+}
+
+
+int day = Prompt("Введите день недели: ");
+if (day >= 1 && day <= 7)
+{
+    if (day <= 5)
+    {
+        Console.WriteLine("Это будний день");
+    }
+    else
+    {
+        Console.WriteLine("Это выходной");
+    }
+}
+else
+{
+    Console.WriteLine("Это не день недели");
+}
