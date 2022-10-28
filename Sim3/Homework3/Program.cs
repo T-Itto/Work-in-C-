236c08f826abebd437edef560c7558be82836b95
Geraschenco Vladislav <xrawen3@gmail.com>
2022-10-28 19:27:14 +0300

    added Homework three Sim three

---
+++ Sim3/Homework3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int Prompt (string msg)
+{
+    System.Console.Write(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = Prompt ("Введите значение N: ");
+for (int i = 0; number >= i; i++)
+{
+    System.Console.WriteLine($"{i}^3 = " + Math.Pow(i, 3));
+}
