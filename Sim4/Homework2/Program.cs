f7b47405611bbdcffa7503168fa931f9a09d3542
Geraschenco Vladislav <xrawen3@gmail.com>
2022-10-28 20:00:33 +0300

    added Homework two Sim four

---
+++ Sim4/Homework2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе. Функция должна на вход принимать число, а выдавать сумму его цифр
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Module(int module)
+{
+    if (module < 0)
+    {
+        module = -module;
+    }
+    return module;
+}
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int NumberDozens(int number) 
+{
+    number = Module(number);
+    int i = 1;
+    while (number >= 10)
+    {
+        number /= 10;
+        i++;
+    }
+    return i;
+}
+
+int value = Prompt("Введите число: ");
+
+int SumValue (int number)
+{
+    int sum = 0;
+    while (number >= NumberDozens(value))
+    {
+        int value = 0;
+        value = value + number % 10;
+        sum = sum + value;
+        number /= 10;
+    }
+    return sum;
+}
+
+System.Console.WriteLine(SumValue(value));
