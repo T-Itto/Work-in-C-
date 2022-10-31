a850f8f5787c459627d99e1c128a45b168e11ad1
Geraschenco Vladislav <xrawen3@gmail.com>
2022-11-01 00:52:21 +0300

    added Homework two Sim five

---
+++ SIm5/Homework2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CrieteRandomArrey(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        int rnd = new Random().Next(10, 100);
+        array[i] = rnd;
+    }
+    return array;
+}
+
+void PrinterArray(int [] array)
+{
+    for (int i = 0; i <= array.Length-1; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int Sum(int[] array)
+{  
+    int sum = 0;
+    for (int i = 0; array.Length >= i+1; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+int[] array = CrieteRandomArrey(new Random().Next(0, 10));
+PrinterArray(array);
+System.Console.WriteLine();
+System.Console.WriteLine(Sum(array));
