6d636991bb31a0f513c17bd4e8a29c7a614bab47
Geraschenco Vladislav <xrawen3@gmail.com>
2022-11-19 15:11:20 +0300

    added Homework one Sim nine

---
+++ Sim9/Homework1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, 
+// которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+Console.Clear();
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void Even(int M, int N)
+{
+    if (N <= 0)
+    {
+        return;
+    }
+
+    if (M < N)
+    {
+        Even(M, N - 1);
+        if (N % 2 == 0)
+        {
+            System.Console.Write(N + " ");
+        }
+    }
+}
+
+int M = Prompt("Введите начало диапозона М: ");
+int N = Prompt("Введите конец диапозона N: ");
+
+Even(M, N);
+System.Console.WriteLine();
